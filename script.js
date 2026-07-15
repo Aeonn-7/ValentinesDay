8ef58989108ca8c155d66fa36e51a10f8afba5bc
Aeonn-7 <lanzdael@gmail.com>
2026-07-15 09:21:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU ALWAYS JEJU').split('')
+    const titles = ('I LOVE YOU ALWAYS').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
